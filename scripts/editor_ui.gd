60b3a2d0a37473aeb2d1288e96992eb6e29137ad
misiekii123 <michalpl2008@interia.pl>
2025-08-03 09:36:37 +0200

    Update editor_ui.gd

--- scripts/editor_ui.gd
+++ scripts/editor_ui.gd
@@ -1,1 +1,5 @@
 extends Control
+
+@onready var intersections_button = $VBoxContainer/IntersectionsButton
+@onready var edges_button = $VBoxContainer/EdgesButton
+@onready var generate_json_button = $VBoxContainer/GenerateJSONButtonButton
