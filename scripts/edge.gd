c6adba33533dd9d1c0228cfa2bc36abf0741682b
misiekii123 <michalpl2008@interia.pl>
2025-08-03 09:59:14 +0200

    Update edge.gd

--- scripts/edge.gd
+++ scripts/edge.gd
@@ -9,4 +9,5 @@ var intersection_a: Intersection
 var intersection_b: Intersection
 
 func _draw() -> void:
+	z_index = -1
 	draw_line(intersection_a.position, intersection_b.position, Color.LIGHT_GRAY, 5.0, true)
