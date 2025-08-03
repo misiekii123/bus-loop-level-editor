b822d1375090c073a0768dfd0b21dc3bbe45af2b
misiekii123 <michalpl2008@interia.pl>
2025-08-03 09:15:12 +0200

    spawning intersections

--- scripts/intersection.gd
+++ scripts/intersection.gd
@@ -3,4 +3,4 @@ extends Node2D
 class_name Intersection
 
 func _draw() -> void:
-	draw_circle(position, 10.0, Color.GRAY, true, -1.0, true)
+	draw_circle(Vector2.ZERO, 10.0, Color.GRAY, true, -1.0, true)
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+var intersection_scene = load("res://scenes/intersection.tscn")
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		var intersection_instance = intersection_scene.instantiate()
+		intersection_instance.position = get_global_mouse_position()
+		add_child(intersection_instance)
