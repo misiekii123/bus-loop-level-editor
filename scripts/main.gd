7679385201f75a85fa77f148ef9a53fe88579f51
misiekii123 <michalpl2008@interia.pl>
2025-08-03 09:25:41 +0200

    deleting intersections support

--- scripts/main.gd
+++ scripts/main.gd
@@ -19,7 +19,15 @@ func _ready() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if current_mode == modes.INTERSECTIONS:
+			var click_pos = get_global_mouse_position()
+
+			for intersection in intersection_list:
+				if intersection.position.distance_to(click_pos) <= 10.0:
+					intersection_list.erase(intersection)
+					intersection.queue_free()
+					return
+
 			var intersection_instance = intersection_scene.instantiate()
-			intersection_instance.position = get_global_mouse_position()
+			intersection_instance.position = click_pos
 			intersection_list.append(intersection_instance)
 			intersections_parent.add_child(intersection_instance)
