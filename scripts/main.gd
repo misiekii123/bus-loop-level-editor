f8806267275cfc62a16508cb2555faae47a86bd7
misiekii123 <michalpl2008@interia.pl>
2025-08-03 09:22:24 +0200

    editor modes support

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,14 +1,25 @@
 extends Node2D
 
+enum modes {
+	INTERSECTIONS,
+	EDGES
+}
+
+var current_mode: modes
+
 var intersection_scene = load("res://scenes/intersection.tscn")
 
 @onready var intersections_parent: Node = $Intersections
 
 var intersection_list: Array[Intersection]
 
+func _ready() -> void:
+	current_mode = modes.INTERSECTIONS
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		var intersection_instance = intersection_scene.instantiate()
-		intersection_instance.position = get_global_mouse_position()
-		intersection_list.append(intersection_instance)
-		intersections_parent.add_child(intersection_instance)
+		if current_mode == modes.INTERSECTIONS:
+			var intersection_instance = intersection_scene.instantiate()
+			intersection_instance.position = get_global_mouse_position()
+			intersection_list.append(intersection_instance)
+			intersections_parent.add_child(intersection_instance)
