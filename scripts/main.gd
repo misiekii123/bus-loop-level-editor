19fef62a06cdb2f04df1be0f44e19fb1da2caa3f
misiekii123 <michalpl2008@interia.pl>
2025-08-03 09:16:56 +0200

    saving intersections on list

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,8 +2,11 @@ extends Node2D
 
 var intersection_scene = load("res://scenes/intersection.tscn")
 
+var intersection_list: Array[Intersection]
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		var intersection_instance = intersection_scene.instantiate()
 		intersection_instance.position = get_global_mouse_position()
+		intersection_list.append(intersection_instance)
 		add_child(intersection_instance)
